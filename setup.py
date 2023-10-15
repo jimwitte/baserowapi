4df09ff66b932f62bd4fb82c3142b3d51ea9078b
Jim Witte <jwitte@illinois.edu>
2023-10-15 17:04:32 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="baserowapi",
+    version="0.1.0b",
+    packages=find_packages(),
+    install_requires=[
+        "pytz>=2023.3.post1",
+        "Requests~=2.31"
+    ],
+    author="James P Witte",
+    author_email="jim@thunderingbison.com",
+    description="API wrapper for Baserow", 
+    long_description=open("README.md").read(),  
+    long_description_content_type="text/markdown",
+    url="https://github.com/jimwitte/baserowapi",
+    license="GPLv3",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires='>=3.10.*',
+)
