649b8cb63091141a85888191922229395b38766c
Jim Witte <jim@thunderingbison.com>
2023-10-14 21:03:10 -0400

    formatting

--- baserowapi/baserow.py
+++ baserowapi/baserow.py
@@ -3,6 +3,7 @@ import logging
 from typing import IO, Union, Dict, Optional, Any
 from baserowapi.models.table import Table
 
+
 class Baserow:
     """
     A client class for interacting with the Baserow API.
@@ -23,11 +24,13 @@ class Baserow:
         503: "Service unavailable at {url}. The server could not process your request in time.",
     }
 
-    def __init__(self, 
-                 url: str = 'https://api.baserow.io', 
-                 token: Optional[str] = None, 
-                 logging_level: int = logging.WARNING, 
-                 log_file: Optional[str] = None) -> None:
+    def __init__(
+        self,
+        url: str = "https://api.baserow.io",
+        token: Optional[str] = None,
+        logging_level: int = logging.WARNING,
+        log_file: Optional[str] = None,
+    ) -> None:
         """
         Initialize a Baserow client.
 
@@ -42,12 +45,11 @@ class Baserow:
         self.token = token
         self.headers: Dict[str, str] = {
             "Authorization": f"Token {self.token}",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
         self.session = requests.Session()
         self.session.headers.update(self.headers)
         self.configure_logging(logging_level, log_file)
-
 
     def configure_logging(self, level: int, log_file: Optional[str]) -> None:
         """
@@ -62,10 +64,9 @@ class Baserow:
 
         logging.basicConfig(
             level=level,
-            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            handlers=handlers
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            handlers=handlers,
         )
-
 
     def __repr__(self) -> str:
         """
@@ -74,7 +75,6 @@ class Baserow:
         :return: A string representing the Baserow client with its base URL.
         """
         return f"Baserow client for base url {self.url}"
-
 
     def get_table(self, table_id: int) -> Table:
         """
@@ -85,14 +85,15 @@ class Baserow:
         """
         return Table(table_id, self)
 
-
-    def make_api_request(self, 
-                     endpoint: str, 
-                     method: str = "GET", 
-                     data: Optional[Dict] = None, 
-                     headers: Optional[Dict[str, str]] = None, 
-                     timeout: int = 10, 
-                     files: Optional[Dict[str, IO[bytes]]] = None) -> Any:
+    def make_api_request(
+        self,
+        endpoint: str,
+        method: str = "GET",
+        data: Optional[Dict] = None,
+        headers: Optional[Dict[str, str]] = None,
+        timeout: int = 10,
+        files: Optional[Dict[str, IO[bytes]]] = None,
+    ) -> Any:
         """
         Make an API request to the specified endpoint.
 
@@ -108,18 +109,21 @@ class Baserow:
 
         url = self.url + endpoint
         combined_headers = self.get_combined_headers(headers)
-        
-        response = self.perform_request(method, url, combined_headers, data, timeout, files)
-        
+
+        response = self.perform_request(
+            method, url, combined_headers, data, timeout, files
+        )
+
         if response.status_code in self.ERROR_MESSAGES:
             error_message = self.ERROR_MESSAGES[response.status_code].format(url=url)
             logger.error(error_message)
             raise Exception(error_message)
-        
+
         return self.parse_response(response, method, url)
 
-
-    def get_combined_headers(self, additional_headers: Optional[Dict[str, str]]) -> Dict[str, str]:
+    def get_combined_headers(
+        self, additional_headers: Optional[Dict[str, str]]
+    ) -> Dict[str, str]:
         """
         Combines the default headers with any additional headers provided.
 
@@ -130,14 +134,15 @@ class Baserow:
             return {**self.headers, **additional_headers}
         return self.headers
 
-
-    def perform_request(self, 
-                        method: str, 
-                        url: str, 
-                        headers: Dict[str, str], 
-                        data: Optional[Dict] = None, 
-                        timeout: int = 10, 
-                        files: Optional[Dict[str, Union[str, IO[bytes]]]] = None) -> requests.Response:
+    def perform_request(
+        self,
+        method: str,
+        url: str,
+        headers: Dict[str, str],
+        data: Optional[Dict] = None,
+        timeout: int = 10,
+        files: Optional[Dict[str, Union[str, IO[bytes]]]] = None,
+    ) -> requests.Response:
         """
         Performs an HTTP request using the given parameters.
 
@@ -146,7 +151,7 @@ class Baserow:
         :param headers: Headers to send with the request.
         :param data: The data payload to send with the request.
         :param timeout: The maximum number of seconds to wait for the server response.
-        :param files: The files to send with the request, if any. The dictionary keys are 
+        :param files: The files to send with the request, if any. The dictionary keys are
                     the form field names, and the values are the file data.
         :return: The server's response to the request.
         :raises requests.exceptions.HTTPError: If the response status code is in the defined ERROR_MESSAGES.
@@ -161,15 +166,19 @@ class Baserow:
             # Conditionally adjust headers for file uploads
             if files:
                 logger.debug(f"API file upload request {files}")
-                headers.pop('Content-Type', None)
+                headers.pop("Content-Type", None)
                 # session.request seems to insist on header "Content: application/json" :-(
                 response = requests.post(url=url, headers=headers, files=files)
             else:
-                response = self.session.request(method=method, url=url, headers=headers, json=data, timeout=timeout)
-            
+                response = self.session.request(
+                    method=method, url=url, headers=headers, json=data, timeout=timeout
+                )
+
             # Checking status code from ERROR_MESSAGES directly
             if response.status_code in self.ERROR_MESSAGES:
-                error_message = self.ERROR_MESSAGES[response.status_code].format(url=url)
+                error_message = self.ERROR_MESSAGES[response.status_code].format(
+                    url=url
+                )
                 logger.error(error_message)
                 raise requests.exceptions.HTTPError(error_message, response=response)
             return response
@@ -178,18 +187,21 @@ class Baserow:
             raise
         except requests.exceptions.RequestException as e:
             # Handle any other exceptions here
-            error_message = f"Unexpected error occurred while making a request to {url}: {e}"
+            error_message = (
+                f"Unexpected error occurred while making a request to {url}: {e}"
+            )
             logger.debug(f"JSON payload: {data}")
             logger.error(error_message)
             raise Exception(error_message)
 
-
-    def parse_response(self, response: requests.Response, method: str, url: str) -> Union[int, Dict[str, Any], str]:
+    def parse_response(
+        self, response: requests.Response, method: str, url: str
+    ) -> Union[int, Dict[str, Any], str]:
         """
-        Parses the response received from an HTTP request. 
+        Parses the response received from an HTTP request.
 
         If the response has a status code of 204, it will return the status code.
-        If the response body is empty and the method is not "DELETE" or the status code is not 204, 
+        If the response body is empty and the method is not "DELETE" or the status code is not 204,
         a warning is logged.
         If the response body contains JSON, it attempts to parse and return the JSON.
         Otherwise, the raw response text is returned.
@@ -203,12 +215,12 @@ class Baserow:
         logger = logging.getLogger(__name__)
         if response.status_code == 204:
             return response.status_code
-        
+
         if not response.text:
             if method != "DELETE" or response.status_code != 204:
                 logger.warning(f"No response body received from {url}")
             return None
-        
+
         try:
             return response.json()
         except ValueError:
